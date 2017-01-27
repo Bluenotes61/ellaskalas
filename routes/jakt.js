848a1f9539b252fcecede6c1c8429cb7385e4234
Magnus Ehlde <magnus@040.se>
2017-01-27 23:45:30 +0100

    Rensat

--- routes/jakt.js
+++
@@ -1,9 +0,0 @@
-
-/*** Render page or redirect to login ***/
-exports.index = function(req, res) {
-  var person = req.query.p;
-  res.render("jakt", {
-    person:person
-  });
-};
-
--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var defaultpage = require("../routes/default.js");
-var jakt = require("../routes/jakt.js");
 
 router.get("/", defaultpage.index);
-router.get("/jakt", jakt.index);
 
 module.exports = router;
